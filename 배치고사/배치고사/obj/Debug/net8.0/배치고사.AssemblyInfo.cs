d7f6566e5ff3ba70c4b5c0ee8900e705318611fd
rlqma1131 <rlqma1131@gmail.com>
2025-05-20 21:37:14 +0900

    feat: TIL 목록 추가 생성

--- 배치고사/배치고사/obj/Debug/net8.0/배치고사.AssemblyInfo.cs
+++ 배치고사/배치고사/obj/Debug/net8.0/배치고사.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("배치고사")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7147b39355391c2707aa9fe5fadd2bdbe0015a68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41df0651f48e1bbb8437405c88d0da06f061b1d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("배치고사")]
 [assembly: System.Reflection.AssemblyTitleAttribute("배치고사")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
